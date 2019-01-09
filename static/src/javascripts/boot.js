08c0890f2484069814ea830cbc5b52fa8dc63ebd
Alex Ware <alex.ware@guardian.co.uk>
2019-01-09 14:22:53 +0000

    Make boot only load commercial.

--- static/src/javascripts/boot.js
+++ static/src/javascripts/boot.js
@@ -4,11 +4,8 @@
 import 'core-js/modules/es7.object.get-own-property-descriptors';
 
 import domready from 'domready';
-import raven from 'lib/raven';
-import { bootStandard } from 'bootstraps/standard/main';
-import config from 'lib/config';
-import { markTime } from 'lib/user-timing';
-import { capturePerfTimings } from 'lib/capture-perf-timings';
+  import config from 'lib/config';
+
 
 // Let webpack know where to get files from
 // __webpack_public_path__ is a special webpack variable
@@ -16,17 +13,11 @@ import { capturePerfTimings } from 'lib/capture-perf-timings';
 // eslint-disable-next-line camelcase,no-undef
 __webpack_public_path__ = `${config.get('page.assetsPath')}javascripts/`;
 
-// Debug preact in DEV
-if (process.env.NODE_ENV !== 'production') {
-    import('preact/debug');
-}
 
 // kick off the app
 const go = () => {
     domready(() => {
-        // 1. boot standard, always
-        markTime('standard boot');
-        bootStandard();
+
 
         // 2. once standard is done, next is commercial
         if (process.env.NODE_ENV !== 'production') {
@@ -44,47 +35,14 @@ const go = () => {
         // Start downloading these ASAP
 
         // eslint-disable-next-line no-nested-ternary
-        const fetchCommercial = config.get('switches.commercial')
-            ? (markTime('commercial request'),
-              import(/* webpackChunkName: "commercial" */ 'bootstraps/commercial'))
-            : Promise.resolve({ bootCommercial: () => {} });
 
-        const fetchEnhanced = window.guardian.isEnhanced
-            ? (markTime('enhanced request'),
-              import(/* webpackChunkName: "enhanced" */ 'bootstraps/enhanced/main'))
-            : Promise.resolve({ bootEnhanced: () => {} });
+              import(/* webpackChunkName: "commercial" */ 'bootstraps/commercial').then(_=>_.bootCommercial())
 
-        raven.context(
-            {
-                tags: {
-                    feature: 'commercial',
-                },
-            },
-            () => {
-                Promise.all([
-                    fetchCommercial.then(({ bootCommercial }) => {
-                        markTime('commercial boot');
-                        return bootCommercial();
-                    }),
-                    fetchEnhanced.then(({ bootEnhanced }) => {
-                        markTime('enhanced boot');
-                        return bootEnhanced();
-                    }),
-                ]).then(() => {
-                    if (document.readyState === 'complete') {
-                        capturePerfTimings();
-                    } else {
-                        window.addEventListener('load', capturePerfTimings);
-                    }
-                });
-            }
-        );
     });
-};
-
-// make sure we've patched the env before running the app
-if (window.guardian.polyfilled) {
-    go();
-} else {
-    window.guardian.onPolyfilled = go;
 }
+
+
+
+
+
+go()
