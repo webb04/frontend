1b2a2bcfd9f9e67b145502ed656f5985d0dd8c4c
vanessa <32312712+vlbee@users.noreply.github.com>
2019-05-30 14:53:46 +0100

    Refactor renderEmailHeadline to handle LinkSnap edge case

--- facia/app/controllers/front/FrontHeadline.scala
+++ facia/app/controllers/front/FrontHeadline.scala
@@ -3,6 +3,7 @@ package controllers.front
 import common.Logging
 import model.Cached.{RevalidatableResult, WithoutRevalidationResult}
 import model.{Cached, PressedPage}
+import model.pressed.LinkSnap
 import play.api.mvc.Results
 
 object FrontHeadline extends Results with Logging {
@@ -10,15 +11,22 @@ object FrontHeadline extends Results with Logging {
   val headlineNotFound: Cached.CacheableResult = WithoutRevalidationResult(NotFound("Could not extract headline from front"))
 
   def renderEmailHeadline(faciaPage: PressedPage): Cached.CacheableResult = {
-    val webTitle = for {
+    val headline = for {
       topCollection <- faciaPage.collections.headOption
       topCurated <- topCollection.curatedPlusBackfillDeduplicated.headOption
-    } yield RevalidatableResult.Ok(topCurated.properties.webTitle)
 
-    webTitle.getOrElse {
-      log.warn(s"headline not found for ${faciaPage.id}")
-      headlineNotFound
+    } yield {
+      RevalidatableResult.Ok(topCurated match {
+        case _: LinkSnap => topCurated.header.headline
+        case _ => topCurated.properties.webTitle
+      })
     }
-  }
 
+    headline.
+      getOrElse {
+        log.warn(s"headline not found for ${faciaPage.id}")
+        headlineNotFound
+      }
+
+  }
 }
