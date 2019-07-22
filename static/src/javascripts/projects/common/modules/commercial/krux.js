67116d0e1cda083e16210eaebcbc20a8938fbb12
Ricardo Costa <ricardo.costa@guardian.co.uk>
2019-07-22 12:30:01 +0100

    Adapted Krux module to work with the CMP Framework

--- static/src/javascripts/projects/common/modules/commercial/krux.js
+++ static/src/javascripts/projects/common/modules/commercial/krux.js
@@ -3,68 +3,41 @@ import config from 'lib/config';
 import { getCookie } from 'lib/cookies';
 import reportError from 'lib/report-error';
 import { local } from 'lib/storage';
-import {
-    getAdConsentState,
-    thirdPartyTrackingAdConsent,
-} from 'common/modules/commercial/ad-prefs.lib';
+import { ConsentManagementPlatform } from 'commercial/modules/cmplib/cmp';
 
-// For flag values, see https://konsole.zendesk.com/hc/en-us/articles/360000754674-JavaScript-Consent-Tag-Spec
-const setConsentFlags = consentFlags => {
-    window.Krux('consent:set', consentFlags, ex => {
-        if (ex) {
-            switch (ex.idv) {
-                case 'no identifier found for user':
-                case 'user opted out via (optout or dnt)':
-                    break; // swallow these as they're harmless
-                default: {
-                    const exStr = Object.keys(ex)
-                        .map(prop => `${prop} -> '${ex[prop]}'`)
-                        .join(', ');
-                    const msg = `KRUX: ${exStr}`;
-                    reportError(new Error(msg), {
-                        feature: 'krux:consent:set',
-                        consentFlags,
-                    });
-                }
-            }
-        }
-    });
-};
+let numRetries = 0;
 
-const enableSegments = () => {
-    setConsentFlags({
+const configureSegments = () => {
+    // For flag values, see https://konsole.zendesk.com/hc/en-us/articles/360000754674-JavaScript-Consent-Tag-Spec
+    const consentFlags = {
         dc: true,
         al: true,
         tg: true,
         cd: false,
         sh: false,
         re: false,
-    });
-};
+    };
 
-const disableSegments = () => {
-    setConsentFlags({
-        dc: false,
-        al: false,
-        tg: false,
-        cd: false,
-        sh: false,
-        re: false,
-    });
-};
-
-let numRetries = 0;
-
-const configureSegments = () => {
     if (window.Krux) {
-        // everything except an explicit denial of consent gives segments
-        const consentToSegments =
-            getAdConsentState(thirdPartyTrackingAdConsent) !== false;
-        if (consentToSegments) {
-            enableSegments();
-        } else {
-            disableSegments();
-        }
+        window.Krux('consent:set', consentFlags, ex => {
+            if (ex) {
+                switch (ex.idv) {
+                    case 'no identifier found for user':
+                    case 'user opted out via (optout or dnt)':
+                        break; // swallow these as they're harmless
+                    default: {
+                        const exStr = Object.keys(ex)
+                            .map(prop => `${prop} -> '${ex[prop]}'`)
+                            .join(', ');
+                        const msg = `KRUX: ${exStr}`;
+                        reportError(new Error(msg), {
+                            feature: 'krux:consent:set',
+                            consentFlags,
+                        });
+                    }
+                }
+            }
+        });
     } else if (numRetries < 20) {
         // give up to 2s for slow networks
         numRetries += 1;
@@ -85,9 +58,10 @@ const retrieve = (n: string): string => {
 };
 
 export const getKruxSegments = (): Array<string> => {
-    const wantPersonalisedAds: boolean =
-        getAdConsentState(thirdPartyTrackingAdConsent) !== false;
-    const segments: string = wantPersonalisedAds ? retrieve('segs') : '';
+    // everything except an explicit denial of consent gives segments
+    const hasConsent: boolean =
+        ConsentManagementPlatform.advertisementConsent() !== false;
+    const segments: string = hasConsent ? retrieve('segs') : '';
     return segments ? segments.split(',') : [];
 };
 
